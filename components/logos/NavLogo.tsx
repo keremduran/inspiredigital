a12277e51da10fb390a7ec80b2226b2e1c373cd9
keremduran <keremduran@live.com>
2022-09-18 15:19:00 -0400

    Logo turned from flat string to Link

--- components/logos/NavLogo.tsx
+++ components/logos/NavLogo.tsx
@@ -1,10 +1,11 @@
 import { Box } from '@chakra-ui/react';
 import React from 'react';
+import NavLink from '../links/NavLink';
 
 const NavLogo = () => {
   return (
-    <Box className='logo' color={'whiteAlpha.900'}>
-      INSPIREDIGITAL&nbsp;
+    <Box className='logo' color={'whiteAlpha.900'} fontWeight={'700'}>
+      <NavLink name={'INSPIREDIGITAL'} href={'/'} />
     </Box>
   );
 };
