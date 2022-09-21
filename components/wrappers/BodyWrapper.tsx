3ea30a5699f60e8705c54ecda64dc54681f8f1e6
keremduran <keremduran@live.com>
2022-09-21 11:17:27 -0400

    Console Log removal

--- components/wrappers/BodyWrapper.tsx
+++ components/wrappers/BodyWrapper.tsx
@@ -5,10 +5,6 @@ import useTheme from '../../hooks/useTheme';
 
 const BodyWrapper = ({ children }: Wrapper) => {
   const theme = useTheme();
-  console.log(theme.mode);
-  console.log(
-    `${theme.mode.toString().toLowerCase()}BodyBg-stacked-waves-haikei.svg`
-  );
 
   return (
     <Box
